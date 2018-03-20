d962b63223eaa0afdcab3784677e0ed28968eb66
xucg <xucg021@163.com>
2018-03-20 16:12:30 +0800

    add some models

--- myMonitor/monitor/admin.py
+++ myMonitor/monitor/admin.py
@@ -16,11 +16,29 @@ class TemplateAdmin(admin.ModelAdmin):
 
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'interval', 'plugin_name')
+    filter_horizontal = ('items', )
+
+class TriggerExpressionInline(admin.TabularInline):
+    model = models.TriggerExpression
+
+class TriggerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'severity', 'enabled')
+    inlines = [TriggerExpressionInline, ]
 
 
+class TriggerExpressionAdmin(admin.ModelAdmin):
+    list_display = ('trigger','service','service_index','specified_index_key','operator_type','data_calc_func','threshold','logic_type')
 
 
 admin.site.register(models.Host, HostAdmin)
 admin.site.register(models.HostGroup, HostGroupAdmin)
 admin.site.register(models.Template, TemplateAdmin)
 admin.site.register(models.Service, ServiceAdmin)
+admin.site.register(models.ServiceIndex)
+admin.site.register(models.Action)
+admin.site.register(models.ActionOperation)
+admin.site.register(models.Trigger, TriggerAdmin)
+admin.site.register(models.TriggerExpression,TriggerExpressionAdmin)
+
+# admin.site.register(models.Classroom)
+# admin.site.register(models.Student)
--- myMonitor/monitor/models.py
+++ myMonitor/monitor/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 
+@python_2_unicode_compatible
 class Host(models.Model):
     name = models.CharField(u'名称', max_length=64, unique=True)
     ip_addr = models.GenericIPAddressField(u'IP地址', unique=True)
@@ -27,6 +29,7 @@ class Host(models.Model):
     def __str__(self):
         return self.name
 
+@python_2_unicode_compatible
 class HostGroup(models.Model):
     name = models.CharField(u'名称', max_length=64, unique=True)
     templates = models.ManyToManyField('Template', blank=True)
@@ -35,24 +38,7 @@ class HostGroup(models.Model):
     def __str__(self):
         return self.name
 
-class Service(models.Model):
-    name = models.CharField(u'服务名称', max_length=64, unique=True)
-    interval = models.IntegerField(u'监控间隔', default=60)
-    plugin_name = models.CharField(u'插件名', max_length=64, default='n/a')
-    # items = models.ManyToManyField('ServiceIndex', verbose_name=u'指标列表', blank=True)
-    has_sub_service = models.BooleanField(u'是否有子服务',default=False, help_text=u'如果一个服务还有独立的子服务 ,选择这个,比如 网卡服务有多个独立的子网卡')
-    memo = models.CharField(u'备注', max_length=128, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-class Template(models.Model):
-    name = models.CharField(u'模板名称', max_length=64, unique=True)
-    services = models.ManyToManyField('Service', verbose_name=u'服务列表')
-
-    def __str__(self):
-        return self.name
-
+@python_2_unicode_compatible
 class ServiceIndex(models.Model):
     name = models.CharField(max_length=64)
     key = models.CharField(max_length=64)
@@ -66,5 +52,118 @@ class ServiceIndex(models.Model):
 
     def __str__(self):
         return '{}.{}'.format(self.name, self.key)
+
+@python_2_unicode_compatible
+class Service(models.Model):
+    name = models.CharField(u'服务名称', max_length=64, unique=True)
+    interval = models.IntegerField(u'监控间隔', default=60)
+    plugin_name = models.CharField(u'插件名', max_length=64, default='n/a')
+    items = models.ManyToManyField('ServiceIndex', verbose_name=u'指标列表', blank=True)
+    has_sub_service = models.BooleanField(u'是否有子服务',default=False, help_text=u'如果一个服务还有独立的子服务 ,选择这个,比如 网卡服务有多个独立的子网卡')
+    memo = models.CharField(u'备注', max_length=128, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
+class Template(models.Model):
+    name = models.CharField(u'模板名称', max_length=64, unique=True)
+    services = models.ManyToManyField('Service', verbose_name=u'服务列表')
+
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
+class TriggerExpression(models.Model):
+    trigger = models.ForeignKey('Trigger', verbose_name=u"所属触发器", on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, verbose_name=u"关联服务", on_delete=models.CASCADE)
+    service_index = models.ForeignKey(ServiceIndex, verbose_name=u"关联服务指标", on_delete=models.CASCADE)
+    specified_index_key = models.CharField(verbose_name=u"只监控专门指定的指标key", max_length=64, blank=True, null=True)
+    operator_type_choices = (('eq', '='), ('lt', '<'), ('gt', '>'))
+    operator_type = models.CharField(u"运算符", choices=operator_type_choices, max_length=32)
+    data_calc_type_choices = (
+        ('avg', 'Average'),
+        ('max', 'Max'),
+        ('hit', 'Hit'),
+        ('last', 'Last'),
+    )
+    data_calc_func = models.CharField(u"数据处理方式", choices=data_calc_type_choices, max_length=64)
+    data_calc_args = models.CharField(u"函数传入参数", help_text=u"若是多个参数,则用,号分开,第一个值是时间", max_length=64)
+    threshold = models.IntegerField(u"阈值")
+    logic_type_choices = (('or', 'OR'), ('and', 'AND'))
+    logic_type = models.CharField(u"与一个条件的逻辑关系", choices=logic_type_choices, max_length=32, blank=True, null=True)
+
+    def __str__(self):
+        return "%s %s(%s(%s))" % (self.service_index, self.operator_type, self.data_calc_func, self.data_calc_args)
+
+@python_2_unicode_compatible
+class Trigger(models.Model):
+    name = models.CharField(u'角发器名称', max_length=64)
+    severity_choices = (
+        (1, 'Information'),
+        (2, 'Warning'),
+        (3, 'Average'),
+        (4, 'High'),
+        (5, 'Disaster'),
+    )
+    severity = models.IntegerField(u'警告级别', choices=severity_choices)
+    enabled = models.BooleanField(default=True)
+    memo = models.TextField(u'备注', blank=True, null=True)
+
+    def __str__(self):
+        return "<serice:%s, severity:%s>" %(self.name,self.get_severity_display())
+
+
+@python_2_unicode_compatible
+class Action(models.Model):
+    '''定义trigger发生后，如何报警'''
+    name = models.CharField(u'名称', max_length=64, unique=True)
+    host_groups = models.ManyToManyField('HostGroup', blank=True)
+    hosts = models.ManyToManyField('Host', blank=True)
+    #........
+    interval = models.IntegerField(u'告警间隔', default=300)
+    operations = models.ManyToManyField('ActionOperation')
+    recover_notice = models.BooleanField(u'故障恢复后发送通知消息', default=True)
+    recover_subject = models.CharField(max_length=128, blank=True, null=True)
+    recover_message = models.TextField(blank=True, null=True)
+    enabled = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+@python_2_unicode_compatible
+class ActionOperation(models.Model):
+    '''报警动作列表'''
+    name = models.CharField(u'名称', max_length=64)
+    step = models.SmallIntegerField(u'第n次告警', default=1, help_text='当trigger触发次数小于这个值时就执行这条记录里报警方式')
+    action_type_choices = (
+        ('email', 'Email'),
+        ('sms', 'SMS'),
+        ('script', 'RunScript'),
+    )
+    action_type = models.CharField(u'动作类型', choices=action_type_choices, default='email', max_length=64)
+    #............
+    _msg_format = 'Host({hostname}, {ip}) service({service_name}) has issue, msg:{msg}'
+    msg_format = models.TextField(u'消息格式', default=_msg_format)
+
+    def __str__(self):
+        return self.name
+
+# class Student(models.Model):
+#     name = models.CharField(max_length=32)
+#     # classroom = models.ForeignKey('Classroom', on_delete=models.CASCADE)
+#     classroom = models.ManyToManyField('Classroom')
+#
+#     def __str__(self):
+#         return self.name
+#
+# class Classroom(models.Model):
+#     name = models.CharField(max_length=20)
+#
+#     def __str__(self):
+#         return self.name
+
 
 
