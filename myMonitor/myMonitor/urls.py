71ced3a26059b2af300263237478bc6762fe09ca
xucg <xucg021@163.com>
2018-03-21 17:20:53 +0800

    add some comments

--- myMonitor/myMonitor/urls.py
+++ myMonitor/myMonitor/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('^$', views.cookie_test),
     url('^api/', include('monitor.api_urls'))   #关于接口api/ 的请求都分发给api_urls处理
 ]
--- myMonitor/myMonitor/views.py
+++ myMonitor/myMonitor/views.py
@@ -2,3 +2,10 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+
+
+def cookie_test(request):
+    response = HttpResponse('This is a cookie test')
+    response.set_cookie('myMonitorId', '102478955')
+    response.set_cookie('myMonitroId2', '212456666')
+    return response
