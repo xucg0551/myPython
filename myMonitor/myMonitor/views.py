eb82fe44b26ebe5a89b52bdae9d4b1c53a563b5e
xucg <xucg021@163.com>
2018-03-21 14:41:20 +0800

    add some comments

--- myMonitor/myMonitor/views.py
+++ myMonitor/myMonitor/views.py
@@ -2,15 +2,3 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
-
-def index(request):
-    print(request.path)
-    return HttpResponseRedirect(reverse('login', args=(1024,)))
-
-def login(request, id):
-    print(request.path)
-    print(request.user)
-    print(request.GET.urlencode)
-    print(request.META)
-    print(request.COOKIES)
-    return HttpResponse(id)
