c0440ce3dee57286baa41d47fa9a196042ec9165
xucg <xucg021@163.com>
2018-03-21 14:18:23 +0800

    add simplify codes

--- myMonitor/myMonitorClient/core/main.py
+++ myMonitor/myMonitorClient/core/main.py
@@ -8,8 +8,8 @@ class CommandLineHandler(object):
         self.help() if len(sys_args) < 2 else self.allocate(sys_args[1])
 
     #根据输入的命令行参数进行反射执行
-    def allocate(self, func_str):
-        getattr(self, func_str)() if hasattr(self, func_str) else self.help()
+    def allocate(self, command):
+        getattr(self, command)() if hasattr(self, command) else self.help()
 
     #启动服务
     def start(self):
