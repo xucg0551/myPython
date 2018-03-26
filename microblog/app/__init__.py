518497a473c1061f7afc3ad541367c9a3cb2cf84
xucg <xucg021@163.com>
2018-03-26 17:44:52 +0800

    add flask+redis+proxy

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -3,10 +3,18 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+lm.login_view = 'login'
 
 from app import views, models
