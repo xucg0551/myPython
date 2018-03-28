df6eed13bde96a6191df43eb501e299e31e79c9d
xucg <xucg021@163.com>
2018-03-28 13:19:25 +0800

    add flask+redis+proxy

--- crawlers/scrapy_demos/douban_imgs/douban_imgs/run_spider.py
+++
@@ -1,3 +0,0 @@
-from scrapy import cmdline
-cmd_str = 'scrapy crawl download_douban'
-cmdline.execute(cmd_str.split(' '))
--- crawlers/scrapy_demos/news/news/main.py
+++
@@ -1,5 +0,0 @@
-from scrapy.cmdline import execute
-import os, sys
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(['scrapy', 'crawl', 'hefei'])
